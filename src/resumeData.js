c2e80b404b010a6b8e6f25c6c4bd655622637b08
unknown <memmedovabdulmecid12@gmail.com>
2022-12-11 19:27:12 +0300

    mecid

--- src/resumeData.js
+++ src/resumeData.js
@@ -42,29 +42,6 @@ let resumeData = {
       }
     ],
     
-    "portfolio":[
-      {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      }
-    ],
- 
   }
   
   export default resumeData
